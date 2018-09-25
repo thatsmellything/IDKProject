e3770d45a88dc1afe2b8d5bc301d7ec373ca1a4b
Jensen Judkins <thesmelliestman@gmail.com>
2018-09-25 09:35:58 -0600

    Removed catch errors but i will put them back in

--- src/controller/IDKController.java
+++ src/controller/IDKController.java
@@ -11,7 +11,7 @@ public class IDKController
 	
 	public void start()
 	{
-		loops();
+		
 		moreInput();
 		
 	//	System.out.println("Standard output for java :D");
@@ -101,68 +101,50 @@ public class IDKController
 	}
 
 	
-	//--------Catch ERRORS------------
-	public boolean validInt(String example)
-	{
-		boolean isValid = false;
-		
-		//Immediately assign create and make a value equal to something. then return that
-		try
-		{
-			Integer.parseInt(example);
-			isValid = true;
-			
-		}
-		
-		catch(NumberFormatException error)
-		{
-			System.out.println("Type in a valid number boi");
-		}
-		return isValid;
-		//always put return at the bottom for the boolean or else you get red death
-	}
-	
-	public boolean validDouble(String example)
-	{
-		boolean isValid = false;
-		
-		try
-		{
-			Double.parseDouble(example);
-			isValid = true;
-		}
-		catch(NumberFormatException error)
-		{
-			System.out.println("Only floating values are allowed for input");
-		}
-		
-		return isValid;
-	}
 	
 	//You can only use == for ints and booleans
 	
 	
 	
-	//While loops need a predefined variable and that variable must change at some point otherwise it will be infinite
-	// loop an dyou will be a sad boi
-	private void loops()
-	{
-		int count = 0;
-		boolean isDone = false;
-		while (!isDone)
+	
+	
+	//--------Catch ERRORS------------
+		public boolean validInt(String example)
 		{
-			count++;
-			JOptionPane.showMessageDialog(null, "Brother may i have some oats");
-			if(count>10)
+			boolean isValid = false;
+			
+			//Immediately assign create and make a value equal to something. then return that
+			try
 			{
-				isDone=true;
+				Integer.parseInt(example);
+				isValid = true;
+				
 			}
+			
+			catch(NumberFormatException error)
+			{
+				System.out.println("Type in a valid number boi");
+			}
+			return isValid;
+			//always put return at the bottom for the boolean or else you get red death
 		}
-		int ab = 0;
-		while (ab <= 10)
+		
+		public boolean validDouble(String example)
 		{
-			JOptionPane.showMessageDialog(null, ab);
-			ab++;
+			boolean isValid = false;
+			
+			try
+			{
+				Double.parseDouble(example);
+				isValid = true;
+			}
+			catch(NumberFormatException error)
+			{
+				System.out.println("Only floating values are allowed for input");
+			}
+			
+			return isValid;
 		}
-	}
+		
+	
 }
